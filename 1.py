e96f45dad10c07fb5705a2f027be3e52fd57fbaa
sj <vhch66@gmail.com>
2022-11-07 08:43:59 +0100

    add wandb

--- 1.py
+++ 1.py
@@ -4,18 +4,23 @@ from lightning_transformers.task.nlp.translation import (
     TranslationTransformer,
     WMT16TranslationDataModule,
 )
+from pytorch_lightning.loggers import WandbLogger
+# wandb_logger = WandbLogger(name='test-11-07', project='translation')
+wandb_logger = WandbLogger(project='translation')
 
 
 if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained(
-        pretrained_model_name_or_path="google/mt5-base"
-    )
+    # tokenizer = AutoTokenizer.from_pretrained(
+    #     pretrained_model_name_or_path="google/mt5-base"
+    # )
+    tokenizer = AutoTokenizer.from_pretrained("google/bert2bert_L-24_wmt_en_de", pad_token="<pad>", eos_token="</s>", bos_token="<s>", unk_token="<unk>")
     model = TranslationTransformer(
-        pretrained_model_name_or_path="google/mt5-base",
-        n_gram=2,
+        # pretrained_model_name_or_path="google/mt5-base",
+        pretrained_model_name_or_path="google/bert2bert_L-24_wmt_en_de",
+        n_gram=1,
         smooth=False,
-        val_target_max_length=142,
-        num_beams=5,
+        val_target_max_length=128,
+        num_beams=4,
         compute_generate_metrics=True,
         load_weights=False,
     )
@@ -31,10 +36,28 @@ if __name__ == "__main__":
         batch_size=24,
     )
 
-    trainer = pl.Trainer(accelerator="auto", devices=[0, 1, 2, 3], max_epochs=10, strategy='ddp')
-    # trainer = pl.Trainer(accelerator="auto", devices=[0, 1, 2, 3], max_epochs=5, strategy='ddp', precision=16)
-    # trainer = pl.Trainer(accelerator="gpu", devices=[0, 1], max_epochs=1, strategy='ddp')
-    # trainer = pl.Trainer(accelerator="auto", devices=[0], max_epochs=1)
+    early_stop_callback = pl.callbacks.EarlyStopping(
+        monitor='val_loss',
+        min_delta=0.00,
+        patience=3,
+        verbose=False,
+        mode='min'
+    )
 
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(save_top_k=5, monitor='val_loss', mode='min')
+
+    trainer = pl.Trainer(
+        fast_dev_run=False,
+        logger=wandb_logger,
+        accelerator="auto",
+        devices=[0, 1, 2, 3],
+        max_epochs=10,
+        strategy='ddp',
+        precision=16,
+        # limit_train_batches=0.05,
+        callbacks=early_stop_callback,
+    )
     trainer.fit(model, dm)
 
+    # trainer = pl.Trainer(accelerator='gpu', devices=[0])
+    # trainer.validate(model, dm)
