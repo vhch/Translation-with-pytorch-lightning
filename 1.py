8d08b49ff9b5c62ab84b395a0714e16f6235f90c
sj <vhch66@gmail.com>
2022-11-23 03:13:30 +0100

    1

--- 1.py
+++ 1.py
@@ -9,22 +9,23 @@ import argparse
 from datetime import datetime
 
 now = datetime.now()
-# wandb_logger = WandbLogger(name=f'{now.date()}-facebook/wmt19-de-en', project='translation')
-wandb_logger = WandbLogger(name=f'{now.date()}-bart-base', project='translation')
+wandb_logger = WandbLogger(name=f'{now.date()}-bart', project='translation-wmt14')
+# wandb_logger = WandbLogger(name=f'{now.date()}-bart-base', project='translation')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--batch', default=128, type=int,
+    parser.add_argument('-b', '--batch', default=200, type=int,
                         help='number of each process batch number')
     args = parser.parse_args()
+    # mname = "bbaaaa/myfork"
+    mname = "facebook/bart-base"
 
     # tokenizer = AutoTokenizer.from_pretrained("google/bert2bert_L-24_wmt_en_de", pad_token="<pad>", eos_token="</s>", bos_token="<s>", unk_token="<unk>")
-    tokenizer = AutoTokenizer.from_pretrained("facebook/bart-base")
+    tokenizer = AutoTokenizer.from_pretrained(mname)
     model = TranslationTransformer(
-        # pretrained_model_name_or_path="google/bert2bert_L-24_wmt_en_de",
-        pretrained_model_name_or_path="facebook/bart-base",
-        val_target_max_length=250,
+        pretrained_model_name_or_path=mname,
+        val_target_max_length=128,
         num_beams=5,
         compute_generate_metrics=True,
         load_weights=False,
@@ -34,11 +35,12 @@ if __name__ == "__main__":
     )
     dm = WMT16TranslationDataModule(
         # WMT translation datasets: ['cs-en', 'de-en', 'fi-en', 'ro-en', 'ru-en', 'tr-en']
+        dataset_name='wmt14',
         dataset_config_name="de-en",
-        source_language="de",
-        target_language="en",
-        max_source_length=250,
-        max_target_length=250,
+        source_language="en",
+        target_language="de",
+        max_source_length=128,
+        max_target_length=128,
         padding="max_length",
         tokenizer=tokenizer,
         batch_size=args.batch,
@@ -52,7 +54,7 @@ if __name__ == "__main__":
         mode='min'
     )
 
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(save_top_k=3, monitor='val_bleu_score', mode='max')
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(save_top_k=5, monitor='val_bleu_score', mode='max')
 
     trainer = pl.Trainer(
         # fast_dev_run=True,
@@ -66,15 +68,14 @@ if __name__ == "__main__":
         # limit_train_batches=0.05,
         # callbacks=[checkpoint_callback, early_stop_callback],
         callbacks=[checkpoint_callback],
-        accumulate_grad_batches=8,
-        # track_grad_norm=2,
+        # accumulate_grad_batches=8,
     )
 
     wandb_logger.watch(model, log="all")
 
     trainer.fit(model, dm)
     trainer.test(model, dm)
-    # trainer.test(model, dm, ckpt_path='/sj/test/translation/39qf3lke/checkpoints/epoch=4-step=44425.ckpt')
+    # trainer.test(model, dm, ckpt_path='/sj/test/translation/1a8wtsfm/checkpoints/epoch=22-step=25530.ckpt')
 
     # trainer = pl.Trainer(accelerator='gpu', devices=[0])
     # trainer.validate(model, dm)
