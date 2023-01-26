e0a36e5f9c59f5a34043b3f445e5d7fd11ba1c4c
sj <vhch66@gmail.com>
2023-01-26 08:22:37 +0100

    init

--- tokenizer.py
+++ tokenizer.py
@@ -2,7 +2,6 @@ from datasets import load_dataset
 
 dataset = load_dataset("wmt14", 'de-en', split='train')
 
-
 def convert_to_features(
     examples
 ):
@@ -12,29 +11,11 @@ def convert_to_features(
 dataset = dataset.map(convert_to_features, batched=True)
 
 from tokenizers import Tokenizer
-from tokenizers.models import BPE
-tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-
-from tokenizers import normalizers
-from tokenizers.normalizers import NFD, Lowercase, StripAccents
-tokenizer.normalizer = normalizers.Sequence([NFD(), Lowercase(), StripAccents()])
+# tokenizer = Tokenizer.from_file("tokenizer.json")
+tokenizer = Tokenizer.from_pretrained("facebook/bart-base")
 
 from tokenizers.trainers import BpeTrainer
-trainer = BpeTrainer(vocab_size=52000, special_tokens=["[BOS]", "[PAD]", "[EOS]", "[UNK]", "[MASK]"])  # Adding [BOS] and [EOS] here
-
-from tokenizers import pre_tokenizers
-from tokenizers.pre_tokenizers import Whitespace, ByteLevel
-# tokenizer.pre_tokenizer = ByteLevel()
-tokenizer.pre_tokenizer = pre_tokenizers.Sequence([Whitespace(), ByteLevel()])
-
-from tokenizers import decoders
-tokenizer.decoder = decoders.ByteLevel()
-
-from tokenizers.processors import TemplateProcessing
-tokenizer.post_processor = TemplateProcessing(
-    single="[BOS] $A [EOS]",
-    special_tokens=[("[BOS]", 0), ("[EOS]", 2)],
-)
+trainer = BpeTrainer(vocab_size=37000, special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>"])  # Adding [BOS] and [EOS] here
 
 tokenizer.train_from_iterator(dataset['tokenizers'], trainer)
 
